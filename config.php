7f9e129fcb9f4a2c399be5a661423f0d15f543b7
MaxZhang <373884384@qq.com>
2021-03-31 22:57:49 +0800

    Initial commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$servername = 'localhost';
+$username = 'xiaotu11';
+$password = 'ioJL48uaFT';
+$dbname = 'xiaotu11';
+$dbport = '3306';
+$conn = new mysqli($servername, $username, $password, $dbname,$dbport); 
+?>
